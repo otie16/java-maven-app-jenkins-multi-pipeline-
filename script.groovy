6bfb48acff6fc62811596745e9b80b7eb262b605
Otobong Godwin <66527770+otie16@users.noreply.github.com>
2024-09-30 15:22:48 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -3,13 +3,14 @@ def buildJar() {
     sh 'mvn package'
 } 
 
-def buildImage() {
+def buildImage(){
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
-    }
+    // Extracting the github credentials from jenkins 
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-repo-credentials', passwordVariable: 'PASS', usernameVariable: 'USER', )]){
+        sh 'docker build -t otobongedoho18361/demo-app:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push otobongedoho18361/demo-app:jma-2.0'
+    } 
 } 
 
 def deployApp() {
