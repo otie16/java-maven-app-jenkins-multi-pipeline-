1b549abadc8fe1f870eb877ef4ed50b24f569de8
Otobong Godwin <66527770+otie16@users.noreply.github.com>
2024-11-13 15:30:24 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,20 +1,20 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
+// def buildJar() {
+//     echo "building the application..."
+//     sh 'mvn package'
+// } 
 
-def buildImage(){
-    echo "building the docker image..."
-    // Extracting the github credentials from jenkins 
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-repo-credentials', passwordVariable: 'PASS', usernameVariable: 'USER', )]){
-        sh 'docker build -t otobongedoho18361/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push otobongedoho18361/demo-app:jma-2.0'
-    } 
-} 
+// def buildImage(){
+//     echo "building the docker image..."
+//     // Extracting the github credentials from jenkins 
+//     withCredentials([usernamePassword(credentialsId: 'dockerhub-repo-credentials', passwordVariable: 'PASS', usernameVariable: 'USER', )]){
+//         sh 'docker build -t otobongedoho18361/demo-app:jma-2.0 .'
+//         sh 'echo $PASS | docker login -u $USER --password-stdin'
+//         sh 'docker push otobongedoho18361/demo-app:jma-2.0'
+//     } 
+// } 
 
-def deployApp() {
-    echo 'deploying the application...'
-} 
+// def deployApp() {
+//     echo 'deploying the application...'
+// } 
 
-return this
+// return this
